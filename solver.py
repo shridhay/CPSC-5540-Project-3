f5794c881c07a80a03f709e948fe9bc23a4d2506
shridhay <47260704+shridhay@users.noreply.github.com>
2025-11-06 17:45:47 -0500

    Add files via upload

---
+++ solver.py
@@ -0,0 +1,15 @@
+from __future__ import annotations
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+import re
+from typing import Self, Union
+import numpy as np
+from fractions import Fraction
+from functools import reduce
+import sys
+import time
+import copy
+
+
+if __name__ == "__main__":
+    pass
