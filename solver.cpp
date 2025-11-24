5428884009500ff08d4a4d7f618cdebfce5ad5f4
shridhay <47260704+shridhay@users.noreply.github.com>
2025-11-23 21:08:12 -0500

    Add files via upload

---
+++ solver.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <unordered_map>
+#include <map>
+#include <stack>
+#include <tuple>
+#include <utility>
+
+using namespace std;
+
+int main(){
+
+    cout << "Hello World" << endl;
+    return 0;
+}
+
