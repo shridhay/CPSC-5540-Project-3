feac3e1728d3fb99b9d0309a6be2d1a394d8cc1b
shridhay <47260704+shridhay@users.noreply.github.com>
2025-11-27 02:42:54 -0500

    Update testcpp.sh

--- testcpp.sh
+++ testcpp.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 # Compile the solver
-g++ -std=c++17 -o solver solver.cpp
+g++ -std=c++17 -O3 -o solver solver.cpp
 chmod +x solver
 
 countFalseNegative=0
 countFalsePositive=0
 
-for i in {1..100}; do
+START_TIME=$SECONDS
+
+for i in {1..300}; do
     f="benchmarks/uf50-218/uf50-0${i}.cnf"
     result=$(./solver "$f")
     if [[ "$result" == *UNSAT* ]]; then
@@ -16,7 +18,7 @@ for i in {1..100}; do
     fi
 done
 
-for i in {1..100}; do
+for i in {1..300}; do
     f="benchmarks/UUF50.218.1000/uuf50-0${i}.cnf"
     result=$(./solver "$f")
     if [[ ! "$result" == *UNSAT* ]]; then
@@ -25,6 +27,10 @@ for i in {1..100}; do
     fi
 done
 
+ELAPSED_TIME=$(($SECONDS - $START_TIME))
+
 echo 
 echo "Total UF50 declared UNSAT: $countFalseNegative"
 echo "Total UUF50 declared SAT: $countFalsePositive"
+echo "Execution time in seconds: $ELAPSED_TIME s"
+echo "$(($ELAPSED_TIME/60)) min $(($ELAPSED_TIME%60)) sec"
