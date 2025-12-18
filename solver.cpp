b57703b2d9636727444b47b54ccdc8588c90ad28
shridhay <47260704+shridhay@users.noreply.github.com>
2025-12-18 03:23:27 -0500

    Fix usage message in main function

--- solver.cpp
+++ solver.cpp
@@ -274,7 +274,7 @@ class SAT {
 
 int main(int argc, char *argv[]){
     if (argc != 2){
-        cout << "Usage: ./solver <path/to/cnf/file.cnf>" << endl;
+        cout << "Usage: ./solver <path/to/file.cnf>" << endl;
         return 1;
     }
     string filename = argv[1];
